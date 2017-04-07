9c187d4677655174773112efdd86e6f52add6b0b
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2017-04-07 00:55:36 -0300

    novo dump do banco

--- db/acampantes.sql
+++ db/acampantes.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: 07-Abr-2017 às 03:28
--- Versão do servidor: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Host: localhost
+-- Generation Time: 07-Abr-2017 às 05:54
+-- Versão do servidor: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `acampantes`
@@ -26,20 +26,21 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `acampantes`
 --
 
-CREATE TABLE `acampantes` (
+CREATE TABLE IF NOT EXISTS `acampantes` (
   `nome` varchar(60) NOT NULL,
-  `conta` varchar(4) DEFAULT NULL,
+  `conta` decimal(4,2) DEFAULT NULL,
   `equipe` varchar(30) CHARACTER SET utf8 NOT NULL,
-  `id` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+`id` int(255) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Extraindo dados da tabela `acampantes`
 --
 
 INSERT INTO `acampantes` (`nome`, `conta`, `equipe`, `id`) VALUES
-('zezinho', '50', 'Adoratubers', 1),
-('Rodrigo', '50', 'Adoratubers', 2);
+('zezinho', '50.00', 'Adoratubers', 1),
+('Rodrigo', '50.00', 'Adoratubers', 2),
+('Juninho', '20.50', 'Adoratubers', 3);
 
 -- --------------------------------------------------------
 
@@ -47,11 +48,11 @@ INSERT INTO `acampantes` (`nome`, `conta`, `equipe`, `id`) VALUES
 -- Estrutura da tabela `historico`
 --
 
-CREATE TABLE `historico` (
-  `id` int(255) NOT NULL,
-  `id_acampantes` int(255) NOT NULL,
-  `id_produtos` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `historico` (
+`id` int(255) NOT NULL,
+  `acampante_id` int(255) NOT NULL,
+  `produto_id` int(255) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 -- --------------------------------------------------------
 
@@ -59,20 +60,20 @@ CREATE TABLE `historico` (
 -- Estrutura da tabela `produtos`
 --
 
-CREATE TABLE `produtos` (
-  `id` int(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS `produtos` (
+`id` int(255) NOT NULL,
   `descricao` varchar(200) NOT NULL,
-  `valor` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `valor` decimal(4,2) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Extraindo dados da tabela `produtos`
 --
 
 INSERT INTO `produtos` (`id`, `descricao`, `valor`) VALUES
-(1, 'Cachorro Quente', 8),
-(2, 'chocolate', 5),
-(3, 'alcaÃ§uz', 4);
+(1, 'Cachorro Quente', '8.00'),
+(2, 'chocolate', '5.00'),
+(3, 'alcaÃ§uz', '4.00');
 
 --
 -- Indexes for dumped tables
@@ -82,19 +83,19 @@ INSERT INTO `produtos` (`id`, `descricao`, `valor`) VALUES
 -- Indexes for table `acampantes`
 --
 ALTER TABLE `acampantes`
-  ADD UNIQUE KEY `id` (`id`);
+ ADD UNIQUE KEY `id` (`id`);
 
 --
 -- Indexes for table `historico`
 --
 ALTER TABLE `historico`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `produtos`
 --
 ALTER TABLE `produtos`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -104,17 +105,17 @@ ALTER TABLE `produtos`
 -- AUTO_INCREMENT for table `acampantes`
 --
 ALTER TABLE `acampantes`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `historico`
 --
 ALTER TABLE `historico`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
