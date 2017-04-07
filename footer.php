eb7a0c4273f2a61203876be5bbdd677225f961ab
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2017-04-06 23:19:11 -0300

    Tabela de lista de acampantes

--- footer.php
+++ footer.php
@@ -1,17 +1,10 @@
-<html>
-	<head> 
-		<link href="css/footer.css" rel="stylesheet">
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/bootstrap-theme.min.css" rel="stylesheet">
-		<link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-		<link href="theme.css" rel="stylesheet">
-	</head>
-	<body>
-	
-		<div class="rodape" align="center">
-			<br><span style='color:white;'>Sistema Cantinha. Versão 1.0. Copyright (C) 2017</span>
-		</div>
-	
-	</body>
-	
-</html>
+<link href="css/footer.css" rel="stylesheet">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/bootstrap-theme.min.css" rel="stylesheet">
+<link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+<link href="theme.css" rel="stylesheet">
+
+<div class="rodape" align="center">
+ <br>
+ <span style='color:white;'>Sistema Cantinha. Versão 1.0. Copyright (C) 2017</span>
+</div>
