dcb22b337eed6e0e0ba2a99bba73d8c80f06f73d
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2017-04-14 01:27:14 -0300

    menu active

--- menu_admin.php
+++ menu_admin.php
@@ -3,10 +3,10 @@
 	<div class="container">
 		<div id="navbar" class="navbar-collapse collapse">
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="administrativo.php">Início</a></li>
-				<li><a href="listagem.php">Acampantes</a></li>
-				<li><a href="historico.php">Histórico</a></li>
-				<li><a href="byebye.php">Sair</a></li>
+				<li <?=strpos($_SERVER["REQUEST_URI"], "administrativo.php")!==false?'class="active"':""?>><a href="administrativo.php">Início</a></li>
+				<li <?=strpos($_SERVER["REQUEST_URI"], "listagem.php")!==false?'class="active"':""?>><a href="listagem.php">Acampantes</a></li>
+				<li <?=strpos($_SERVER["REQUEST_URI"], "historico.php")!==false?'class="active"':""?>><a href="historico.php">Histórico</a></li>
+				<li <?=strpos($_SERVER["REQUEST_URI"], "byebye.php")!==false?'class="active"':""?>><a href="byebye.php">Sair</a></li>
 			</ul>
 		</div>
 	</div>
