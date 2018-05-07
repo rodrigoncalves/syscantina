2aaf989041e25a86e662d162d6dc0bff347ae9a6
Rodrigo Gonçalves <rgoncalves@esec.com.br>
2018-05-07 15:37:52 -0300

    correção bmsg de rro ao excluir acampante sem histórico

--- excluir_acampante.php
+++ excluir_acampante.php
@@ -1,8 +1,8 @@
 <?php
 	include_once("conexao.php");
 	$id=$_GET["id"];
-	mysqli_query($con, "DELETE FROM acampantes WHERE id=".$id);
 	mysqli_query($con, "DELETE FROM historico WHERE acampante_id=".$id);
+	mysqli_query($con, "DELETE FROM acampantes WHERE id=".$id);
 
 	echo "<script>";
 	if (mysqli_affected_rows($con) > 0) {
