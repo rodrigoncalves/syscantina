1f1026531adefd3f9634285bd9c4f1599d4f7651
Rodrigo Gonçalves <rgoncalves@esec.com.br>
2018-05-10 21:14:30 -0300

    Insert equipes

---
+++ db/insert_equipes.sql
@@ -0,0 +1,16 @@
+SET NAMES 'utf8' COLLATE 'utf8_general_ci';
+INSERT INTO `equipes` (`id`, `nome`) VALUES
+(1, 'Rúben'),
+(2, 'Simeão'),
+(3, 'Levi'),
+(4, 'Judá'),
+(5, 'Dã'),
+(6, 'Naftali'),
+(7, 'Gade'),
+(8, 'Aser'),
+(9, 'Issacar'),
+(10, 'Zebulom'),
+(11, 'José'),
+(12, 'Benjamim'),
+(13, 'COLABORADOR');
+
