ab1470b80ffbda8f39b42c80e968778103e3f170
Rodrigo Gonçalves <rgoncalves@esec.com.br>
2018-05-11 17:25:52 -0300

    Mostrar total arrecadado e a quitar

--- listagem.php
+++ listagem.php
@@ -39,12 +39,14 @@
 
 				<tbody>
 					<?php $num=1; ?>
-					<?php $total=0; ?>
+					<?php $total_arrecadado=0; ?>
+					<?php $total_quitar=0; ?>
 					<?php while($acampante = mysqli_fetch_array($acampantes)) { ?>
 						<?php
 							$sql=mysqli_query($con, "SELECT * FROM equipes WHERE id=".$acampante['equipe_id']);
 							$acampante['equipe']=mysqli_fetch_array($sql);
-							$total = $total + ($acampante['quitado'] ? 0 : $acampante['conta']);
+							$total_arrecadado += $acampante['conta'];
+							$total_quitar += ($acampante['quitado'] ? 0 : $acampante['saldo']);
 						?>
 						<tr <?=$acampante['quitado']?'class="success"':''?>>
 							<td align="center"><?=$num++?></td>
@@ -60,21 +62,21 @@
 								<?='R$ '.number_format($acampante['conta'], 2, ',', '.')?></td>
 							<td align="center"<?=$acampante['saldo']<=0 ? " style='color:red;'" : ""?>>
 								<?='R$ '.number_format($acampante['saldo'], 2, ',', '.')?></td>
-							<td align="center"><a class="btn btn-warning btn-xs" title="Comprar" href="form_compra.php?id=<?=$acampante['id']?>" <?=$acampante['quitado']?"disabled":""?>><i class="fa fa-shopping-cart"></i></a></td>
+							<td align="center"><a class="btn btn-warning btn-xs" title="Comprar" <?=$acampante['quitado']?"disabled":"href=form_compra.php?id=".$acampante['id']?>><i class="fa fa-shopping-cart"></i></a></td>
 							<td align="center"><a class="btn btn-success btn-xs" title="Histórico" href="historico.php?id=<?=$acampante['id']?>"><i class="fa fa-history"></i></a></td>
 							<td align="center"><a class="btn btn-primary btn-xs" title="Editar" href="form_acampante.php?id=<?=$acampante['id']?>"><i class="fa fa-pencil-square-o"></i></a></td>
 							<td align="center"><a class="btn btn-danger btn-xs" title="Excluir" href="excluir_acampante.php?id=<?=$acampante['id']?>" onclick="return confirm('Excluir um acampante implica em excluir tambem seus registros de compra. Tem certeza que deseja continuar?');"><i class="fa fa-trash-o"></i></a></td>
 							<?php if ($acampante['quitado']) { ?>
 								<td align="center"><a class="btn btn-danger btn-xs" title="Desfazer quitar" href="quitar_acampante.php?acampante_id=<?=$acampante['id']?>&equipe_id=<?=$acampante['equipe_id']?>&source=<?=$source?>" onclick="return confirm('Deseja desfazer a marcação de quitado deste acampante?');"><i class="fa fa-check"></i></a></td>
 							<?php } else { ?>
-								<td align="center"><a class="btn btn-success btn-xs" title="Quitar" href="quitar_acampante.php?acampante_id=<?=$acampante['id']?>&equipe_id=<?=$acampante['equipe_id']?>&source=<?=$source?>" onclick="return confirm('Deseja marcar este acampante como quitado?');"><i class="fa fa-check"></i></a></td>
+								<td align="center"><a class="btn btn-success btn-xs" title="Finalizar" href="quitar_acampante.php?acampante_id=<?=$acampante['id']?>&equipe_id=<?=$acampante['equipe_id']?>&source=<?=$source?>" onclick="return confirm('Deseja marcar este acampante como quitado?');"><i class="fa fa-check"></i></a></td>
 							<?php } ?>
 						</tr>
 					<?php } ?>
 				</tbody>
 				<tfoot>
-					<tr><th colspan="9">Total: <?='R$ '.number_format($total, 2, ',', '.')?></th></tr>
-					<tr><th colspan="9">Total: <?='R$ '.number_format($total, 2, ',', '.')?></th></tr>
+					<tr><th colspan="9">Total arrecadado: <?='R$ '.number_format($total_arrecadado, 2, ',', '.')?></th></tr>
+					<tr><th colspan="9">Total a quitar: <?='R$ '.number_format($total_quitar, 2, ',', '.')?></th></tr>
 				</tfoot>
 			</table>
 		</div>
