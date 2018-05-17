f4efce6e0e1a9a0eaf9dd42f20c1415e5f589974
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2018-05-17 00:11:56 -0300

    Correção filtro de pesquisa

--- js/search.js
+++ js/search.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-  $("#search").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#myTable tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
+	$("#search").on("keyup", function() {
+		var value = $(this).val().toLowerCase();
+		$("#myTable tr").filter(function() {
+			console.log($(this).text().toLowerCase());
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+		});
+	});
 });
